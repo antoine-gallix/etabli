88920ef240d0c34875e332751286d182146f6905
Antoine Gallix <antoine.gallix@a2ei.org>
2023-10-04 14:00:37 +0200

    call logging function

--- etabli/inspect.py
+++ etabli/inspect.py
@@ -1,5 +1,18 @@
 import inspect
+from functools import wraps
 
 
 def calling_function():
-    return inspect.stack()[1].function
+    return inspect.stack()[2].function
+
+def print_call(func):
+    """Print function call string"""
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        args_string = ",".join(repr(arg) for arg in args)
+        kwargs_string = ",".join(f"{k}={repr(v)}" for k, v in kwargs.items())
+
+        print(f"{func.__name__}({args_string},{kwargs_string})")
+        return func(*args, **kwargs)
+
+    return wrapped
