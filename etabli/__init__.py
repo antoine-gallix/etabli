55b4d45d6a9038f4c9318192fdb583f5cba35863
Antoine Gallix <antoine.gallix@a2ei.org>
2023-10-04 13:59:46 +0200

    format

--- etabli/__init__.py
+++ etabli/__init__.py
@@ -1,18 +1,18 @@
-import os
+# import os
 
 # all those imports allows `from dev_tools import *` and have a lot of things in
 # the namespace to quick debug
-from pdb import set_trace as bp  # noqa
-from pprint import pformat as pf  # noqa
-from pprint import pprint as ppr  # noqa
+# from pdb import set_trace as bp  # noqa
+# from pprint import pformat as pf  # noqa
+# from pprint import pprint as ppr  # noqa
 
-from .clock import monitor_run  # noqa
-from .debug import print_factory  # noqa; noqa
-from .debug import File_Writer, Printer, arguments,  status, unbuffered_file  # noqa
-from .dquery import *  # noqa
-from .persistance import *  # noqa
-from .printing import *  # noqa
-from .progress import *  # noqa
+# from .clock import monitor_run  # noqa
+# from .debug import print_factory  # noqa; noqa
+# from .debug import File_Writer, Printer, arguments,  status, unbuffered_file  # noqa
+# from .dquery import *  # noqa
+# from .persistance import *  # noqa
+# from .printing import *  # noqa
+# from .progress import *  # noqa
 # from .viewer import view, view_model  # noqa
 
 # debug_print_filename = os.environ.get("DEBUG_LOG_FILE")
@@ -25,12 +25,12 @@ from .progress import *  # noqa
 
 # ic_config = {"prefix": ""}
 # ic.configureOutput(**ic_config)
-# 
-# 
+#
+#
 # def ic_log_to_file():
-    # ic_config.update({"outputFunction": debug_print_file.write})
-    # ic.configureOutput(**ic_config)
+# ic_config.update({"outputFunction": debug_print_file.write})
+# ic.configureOutput(**ic_config)
 
 
 # a printer with indentation
-p = Printer()
+# p = Printer()
