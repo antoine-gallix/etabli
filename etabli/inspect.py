69a40d50b84c3b691ab78646b6d47a0820778804
Antoine Gallix <antoine.gallix@a2ei.org>
2023-10-11 17:27:28 +0200

    rename cli tool

--- etabli/inspect.py
+++ etabli/inspect.py
@@ -1,7 +1,13 @@
 import inspect
+import sys
 from functools import wraps
+from logging import getLogger
 
 from rich import print
+
+from etabli.persistance import dump_pickle
+
+logger = getLogger(__name__)
 
 
 def calling_function():
@@ -19,5 +25,19 @@ def print_call(func):
         result = func(*args, **kwargs)
         print(f"{func.__name__}({args_string},{kwargs_string}) -> {result}")
         return result
+
+    return wrapped
+
+
+def save_call(func):
+    @wraps(func)
+    def wrapped(*args, save_replay=True, **kwargs):
+        ret = func(*args, **kwargs)
+        if save_replay:
+            replay = (func.__module__, func.__name__, args, kwargs)
+            dump_pickle(replay, "replay.pkl")
+            logger.critical("function call saved, aborting everything")
+            sys.exit()
+        return ret
 
     return wrapped
