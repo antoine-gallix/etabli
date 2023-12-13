edfa19fe4aeba9e686a5d771fe7dd1f1ead639fc
Antoine Gallix <antoine.gallix@a2ei.org>
2023-12-13 23:19:39 +0100

    timestamp tool

--- scripts/cli.py
+++ scripts/cli.py
@@ -24,3 +24,27 @@ def replay_call():
     import os
 
     os.execvp("ipython", ["ipython", "-i", "-m", "etabli.replay"])
+
+
+@main.command
+@click.option("--precision", "-p", type=click.Choice(("s", "ms", "us")), default="s")
+def timestamp(precision):
+    import arrow
+
+    timestamp = arrow.utcnow().timestamp()
+    match precision:
+        case "s":
+            factor = 1
+        case "ms":
+            factor = 10e3
+        case "us":
+            factor = 10e6
+    print(int(timestamp * factor))
+
+
+@main.command
+@click.argument("timestamp")
+def read_timestamp(timestamp):
+    import arrow
+
+    print(arrow.Arrow.fromtimestamp(timestamp))
